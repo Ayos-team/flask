b0dea2b10e9d066257d897a128be0056396c9761
KevSornet <kevin.sornet@obf.ateneo.edu>
2021-12-29 12:57:31 +0800

    testing stuff

--- digitalcafe/database.py
+++ digitalcafe/database.py
@@ -1,7 +1,10 @@
 products = {
     100: {"name":"Americano","price":125},
-    200: {"name":"Brewed Coffee","price":100},
+    200: {"name":"Brewed Coffee","price":110},
     300: {"name":"Cappuccino","price":120},
+    400: {"name":"Espresso","price":120},
+    500: {"name":"Latte","price":140},
+    600: {"name":"Cold Brew","price":200},
     1000: {"name":"Tiramisu","price":150},
     1100: {"name":"Red Velvet","price":130},
     1200: {"name":"Mango Cream Pie","price":200}
@@ -13,6 +16,30 @@ branches = {
     3: {"name":"Eastwood","phonenumber":"09179990002"},
     4: {"name":"Tiendesitas","phonenumber":"09179990003"},
     5: {"name":"Arcovia","phonenumber":"09179990004"},
+}
+
+users = {
+    "chums@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Matthew",
+                         "last_name":"Uy"},
+    "joben@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Joben",
+                         "last_name":"Ilagan"},
+    "bong@example.com":{"password":"Ch@ng3m3!",
+                        "first_name":"Bong",
+                        "last_name":"Olpoc"},
+    "joaqs@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Joaqs",
+                         "last_name":"Gonzales"},
+    "gihoe@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Gio",
+                         "last_name":"Hernandez"},
+    "vic@example.com":{"password":"Ch@ng3m3!",
+                       "first_name":"Vic",
+                       "last_name":"Reventar"},
+    "joe@example.com":{"password":"Ch@ng3m3!",
+                       "first_name":"Joe",
+                       "last_name":"Ilagan"},
 }
 
 def get_product(code):
@@ -40,3 +67,15 @@ def get_branches():
         branch_list.append(branch)
 
     return branch_list
+
+def get_user(username):
+    try:
+       return users[username]
+    except KeyError:
+       return None
+
+def get_pass(username,password):
+    try:
+       return users[username][password]
+    except KeyError:
+       return None
